95f85351c508993fce0bed5d946a17702d115036
André Solak <akasolak@gmail.com>
2024-07-22 14:17:32 -0300

    Create Apollo client in `webs/app`

---
+++ apps/web/app/_lib/apollo-provider.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { HttpLink, ApolloLink } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloNextAppProvider,
+  InMemoryCache,
+  SSRMultipartLink
+} from "@apollo/experimental-nextjs-app-support";
+
+
+function makeClient() {
+  const httpLink = new HttpLink({
+      uri: "http://localhost:4000",
+  });
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
